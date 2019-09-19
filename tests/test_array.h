18cb53b41eede5986adb9d657d4eacf689c72f7f
Philipp Zettl <zettl.philipp@gmail.com>
2019-09-19 21:04:45 +0200

    remove flaky test (flaky for ci)

--- tests/test_array.h
+++ tests/test_array.h
@@ -8,12 +8,10 @@
 void arrayAllocTest();
 void arrayAssignTest();
 void arrayFreeTest();
-void testArray();
 
 void testArray(){
   arrayAllocTest();
   arrayAssignTest();
-  arrayFreeTest();
 }
 
 void arrayAllocTest(){
@@ -37,25 +35,4 @@ void arrayAssignTest(){
   printf("array::assignVals working.\n");
 }
 
-void arrayFreeTest(){
-  double *q;
-  q = allocArray(q, 1);
-  *(q) = 1;
-  const double *preAddr = q;
-  assert(q == preAddr);
-  assert(q[0] == 1);
-  free(q);
-  q = NULL;
-  q = allocArray(q, 2);
-  *(q) = 2;
-  *(q+1) = 3;
-  assert(q != preAddr);
-  assert(q[0] == 2);
-  assert(q[1] == 3);
-#if DEBUG
-  printf("%x != %x\n", preAddr, q);
-#endif
-  free(q);
-  printf("array::free working.\n");
-}
 
