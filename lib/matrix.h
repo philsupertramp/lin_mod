f97aeee2fa918d885e023fee50a4cc8056b7934f
Philipp Zettl <zettl.philipp@gmail.com>
2019-09-28 14:33:33 +0200

    add license

--- lib/matrix.h
+++ lib/matrix.h
@@ -1,10 +1,32 @@
+/**
+ * File              : matrix.h
+ * Author            : Philipp Zettl <philipp.zettl@godesteem.de>
+ * Date              : 28.09.2019
+ * Last Modified Date: 28.09.2019
+ * Last Modified By  : Philipp Zettl <philipp.zettl@godesteem.de>
+ */
+/**
+ * lib/matrix.h
+ * Copyright (c) 2019 Philipp Zettl <philipp.zettl@godesteem.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef MATRIX_H
 #define MATRIX_H
 
-//#include "styling.h"
 #include "array.h"
 
-//#include "vector.h"
 #define u_int unsigned int
 
 typedef struct matrix {
@@ -16,7 +38,6 @@ typedef struct vector {
   double *_e;
   u_int size;
 } vector;
-//#define vector double *
 
 matrix initMatrix(matrix, int, int);
 vector mVecMultiplication(matrix mat, vector vec);
@@ -28,22 +49,4 @@ matrix mScalarMult(matrix, double);
 matrix mMatMult(matrix, matrix);
 matrix transpose(matrix);
 
-/*
-typedef struct matrix {
-  vector **items;
-  int columns;
-  int rows;
-  int storageSize;
-} matrix;
-
-void mat_init(int, ...);
-int mat_col(matrix *);
-int mat_row(matrix *);
-static void mat_resize(matrix *, int);
-void mat_add(matrix *, vector *);
-void mat_set(matrix *, int, int, double*);
-vector *mat_get(matrix *, int);
-void mat_delete(matrix *, int);
-void mat_free(matrix *);
-*/
 #endif
